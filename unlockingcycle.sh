459d6417271c843e2abda274bf4fd0a08b22c32f
Ishan Rai <ishanrai05@gmail.com>
2020-06-18 08:48:02 +0530

    Lint shell scripts

--- unlockingcycle.sh
+++ unlockingcycle.sh
@@ -1,11 +1,14 @@
 BASE_DIR=/data/unified/WmAgentScripts/
+# shellcheck disable=SC2034
 HTML_DIR=/var/www/html/unified/
 
-lock_name=`echo $BASH_SOURCE | cut -f 1 -d "."`.lock
-source $BASE_DIR/cycle_common.sh $lock_name
+# shellcheck disable=SC2128
+lock_name=$(echo "$BASH_SOURCE" | cut -f 1 -d ".").lock
+# shellcheck disable=SC1090
+source $BASE_DIR/cycle_common.sh "$lock_name"
 
 ## unlock dataset that can be unlocked and set status along
 $BASE_DIR/cWrap.sh Unified/lockor.py
 
-rm -f $lock_name
+rm -f "$lock_name"
 
